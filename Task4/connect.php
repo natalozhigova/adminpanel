52644ff44ca432aa87726dc850b90b1d44bf7efb
natalozhigova <47795878+natalozhigova@users.noreply.github.com>
2020-10-20 14:00:37 +0300

    Update connect.php

--- Task4/connect.php
+++ Task4/connect.php
@@ -1,4 +1,4 @@
 <?php
-$connection=mysqli_connect('localhost','root','root');
+$connection=mysqli_connect('https://myadminpanel.herokuapp.com/','b00fd9f1a2bb8e','94d3d6e0');
 $select_db = mysqli_select_db($connection, 'admin');
 
